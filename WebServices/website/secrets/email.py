70696e491d3987c6493bbc8723c1ea37693cad33
xcrespo <xabi.crespo@humsat.org>
2014-09-18 18:55:54 +0200

    Folder secrets included

---
+++ WebServices/website/secrets/email.py
@@ -0,0 +1,32 @@
+"""
+   Copyright 2014 Xabier Crespo Álvarez
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+:Author:
+    Xabier Crespo Álvarez (xabicrespog@gmail.com)
+"""
+__author__ = 'xabicrespog@gmail.com'
+
+
+"""
+	This file contains the parameters used by Django to send emails when
+	registering users. 
+"""
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dummy@gmail.com'
+EMAIL_HOST_PASSWORD = 'dfasdummyData'
+EMAIL_USE_TLS = 'True'
