f5a80e7c5f13dfbfaf8eabc26bf5eb511700100a
satnet <satnet@satnet-dev.calpoly.edu>
2014-09-21 11:20:02 -0700

    Installation script updated, full installation supported

---
+++ scripts/django-secret-key-generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+"""
+Two things are wrong with Django's default `SECRET_KEY` system:
+ 
+1. It is not random but pseudo-random
+2. It saves and displays the SECRET_KEY in `settings.py`
+ 
+This snippet
+1. uses `SystemRandom()` instead to generate a random key
+2. saves a local `secret.txt`
+ 
+The result is a random and safely hidden `SECRET_KEY`.
+"""
+
+import os, random
+
+SECRET_KEY = ''.join([random.SystemRandom().choice(
+    'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+) for i in range(50)])
+
+print SECRET_KEY
